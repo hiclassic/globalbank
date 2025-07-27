ac3d50ba2ba0ab4095b14715d6056379ffa9bbfb
hiclassic <hares.ahmed.319@gmail.com>
2025-07-28 01:22:40 +0600

    AccountController

---
+++ app/controllers/accountController.php
@@ -0,0 +1,15 @@
+<?php
+require_once __DIR__ . '/../models/Account.php';
+
+class AccountController {
+    public function createAccount($user_id, $currency) {
+        $account = new Account();
+        return $account->create($user_id, $currency);
+    }
+
+    public function getAccounts($user_id) {
+        $account = new Account();
+        return $account->getByUser($user_id);
+    }
+}
+?>
