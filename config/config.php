45ec119a05b09ca9422e12017d77251033790ccf
hiclassic <hares.ahmed.319@gmail.com>
2025-07-28 02:01:56 +0600

    config

---
+++ config/config.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'db' => [
+        'host' => 'localhost',
+        'name' => 'globalbank',
+        'user' => 'hiclassic',
+        'pass' => '2997',
+        'charset' => 'utf8mb4'
+    ],
+    'app' => [
+        'debug' => true,
+        'jwt_secret' => 'YOUR_SECRET_KEY'
+    ]
+];
+
+?>
